d60255c2bdd92e1c0918af5efc81608e4d63fea5
Reinhard234 <stopie414@gmail.com>
2021-10-22 20:29:54 +0200

    latest version of HTML, CSS and JS

---
+++ js/checkoutScript.js
@@ -0,0 +1,40 @@
+var Dot1 = document.getElementById("dot1");
+var Dot2 = document.getElementById("dot2");
+var Dot3 = document.getElementById("dot3");
+var DeliveryForm = document.getElementById("deliveryForm");
+var PaymentForm = document.getElementById("PaymentForm");
+var ConfirmationSection = document.getElementById("ConfirmationSection");
+
+
+
+
+function checkoutClick(x) {
+	switch(x) {
+    	case 0:
+    	{
+    		console.log("hello");
+			Dot1.classList.remove("currentlyActive");
+			Dot1.classList.add("completed");
+
+			Dot2.classList.remove("uncompleted");
+			Dot2.classList.add("currentlyActive");
+			DeliveryForm.classList.add("hideFormPart");
+			PaymentForm.classList.remove("hideFormPart");
+			break;
+    	}
+
+    	case 1:
+    	{
+    		console.log("hello");
+			Dot2.classList.remove("currentlyActive");
+			Dot2.classList.add("completed");
+
+			Dot3.classList.remove("uncompleted");
+			Dot3.classList.add("currentlyActive");
+			PaymentForm.classList.add("hideFormPart");
+			ConfirmationSection.classList.remove("hideFormPart");
+			break;
+    	}
+			
+	}
+}
--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,59 @@
 //
 // Scripts
 // 
+var UIUX = document.getElementById("UIUX");
+var Web = document.getElementById("Web");
+var Motion = document.getElementById("Motion");
+var JodyImage = document.getElementById("JodyTopImage");
 
 
-//document.getElementById("top-link").style.display = "none";
-if (document.getElementById("test").className == "active") 
+
+$(window).on('load', function()
 {
-    console.log("yeah");
-}
-//document.getElementById("myDIV").classList.contains("mystyle");
+  $(".loader").delay(2000).fadeOut("slow");
+  $("#overlayer").delay(2000).fadeOut("slow");
+
+});
+
+history.scrollRestoration = "manual";
+
+$(window).on('beforeunload', function()
+{
+  $(window).scrollTop(0)
+
+});
+
+$(this).scrollTop(0);
+
 $("#top-link").hide();
 
 $(document).ready( function() {
     $("#top-link").hide(); //hide your div initially
     var topOfOthDiv = $("#Projects").offset().top;
     $(window).scroll(function() {
-        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-            $("#top-link").show(); //reached the desired point -- show div
+        if($(window).scrollTop() > topOfOthDiv) 
+        { //scrolled past the other div?
+            $("#top-link").show(); //reached the desired point -- show div    
         }
-        else{$("#top-link").hide();}
+        else
+        {
+            $("#top-link").hide();
+            
+        }
     });
+});
+
+
+$(window).scroll(function() {
+      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop < 100) {
+            
+            $("#down-arrow").fadeIn();
+      }
+      else {
+
+          $("#down-arrow").fadeOut(); 
+      }
 });
 
 
@@ -107,4 +141,145 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+  
+
 });
+
+
+function hideTopImage(x) {
+  switch(x) {
+    case 0:
+      UIUX.style.opacity = "0%";
+      break;
+    case 1:
+      Web.style.opacity = "0%";
+      break;
+    case 2:
+      Motion.style.opacity = "0%";
+      break;
+    default:
+      UIUX.style.opacity = "0%";
+    }
+  
+}
+
+function showTopImage(x) {
+  switch(x) {
+    case 0:
+      UIUX.style.opacity = "100%";
+      break;
+    case 1:
+      Web.style.opacity = "100%";
+      break;
+    case 2:
+      Motion.style.opacity = "100%";
+      break;
+    default:
+      UIUX.style.opacity = "100%";
+    }
+}
+
+// function hideTopMemberImage(x) {
+//   switch(x) { 
+//     case 0:
+//       JodyImage.style.opacity = "0%";
+//       break;
+//     case 1:
+//       Web.style.opacity = "0%";
+//       break;
+//     case 2:
+//       Motion.style.opacity = "0%";
+//       break;
+//     case 3:
+//       Motion.style.opacity = "0%";
+//       break;
+//     default:
+//       UIUX.style.opacity = "0%";
+//     }
+  
+// }
+
+// function showTopMemberImage(x) {
+//   switch(x) {
+//     case 0:
+//       JodyImage.style.opacity = "100%";
+//       break;
+//     case 1:
+//       Web.style.opacity = "100%";
+//       break;
+//     case 2:
+//       Motion.style.opacity = "100%";
+//       break;
+//     default:
+//       UIUX.style.opacity = "100%";
+//     }
+// }
+
+
+//move jody details to the right
+
+$("#jody").hover(function(){
+    //alert("hover");
+    $("#JodyDetails").animate({marginLeft: "120px"}, 0.5 ,"linear");
+
+});
+
+$("#jody").mouseleave(function(){
+
+    $("#JodyDetails").animate({marginLeft: "0px"}, 0.5 ,"linear");
+
+});
+
+$("#irone").hover(function(){
+    //alert("hover");
+    $("#IroneDetails").animate({marginLeft: "-120px"}, 0.5 ,"linear");
+
+});
+
+$("#irone").mouseleave(function(){
+
+    $("#IroneDetails").animate({marginLeft: "0px"}, 0.5 ,"linear");
+
+});
+
+$("#claire").hover(function(){
+    //alert("hover");
+    $("#ClaireDetails").animate({marginLeft: "120px"}, 0.5 ,"linear");
+
+});
+
+$("#claire").mouseleave(function(){
+
+    $("#ClaireDetails").animate({marginLeft: "0px"}, 0.5 ,"linear");
+
+});
+
+$("#reinhard").hover(function(){
+    //alert("hover");
+    $("#ReinhardDetails").animate({marginLeft: "-120px"}, 0.5 ,"linear");
+
+});
+
+$("#reinhard").mouseleave(function(){
+
+    $("#ReinhardDetails").animate({marginLeft: "0px"}, 0.5 ,"linear");
+
+});
+
+
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector(".parallax");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
