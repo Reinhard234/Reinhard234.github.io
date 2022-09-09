e9f06127074df3ff769acbb36f3e5b58ec1aab46
Reinhard234 <stopie414@gmail.com>
2022-09-09 16:51:34 +0200

    started afresh

--- js/scripts.js
+++ js/scripts.js
@@ -287,9 +287,9 @@ $("#reinhard").mouseleave(function(){
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.0001}% ${50 - (_mouseY - _h) * 0.0001}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.0002}% ${50 - (_mouseY - _h) * 0.0002}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.0006}% ${50 - (_mouseY - _h) * 0.0006}%`;
         let x = `${_depth3}, ${_depth2}, ${_depth1}`;
         //console.log(x);
         elem.style.backgroundPosition = x;
