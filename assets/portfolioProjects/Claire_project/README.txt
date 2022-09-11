a00f2103e65c2b43b254b2cd973f19dad6ae1aa3
Reinhard234 <stopie414@gmail.com>
2022-09-11 09:55:56 +0200

    adding projects and landing page

--- assets/portfolioProjects/Claire_project/README.txt
+++
@@ -1,3 +0,0 @@
-The project can be opened via final.html
-The final.html should be able to play in any browser.
-If there are problems, I suggest using Edge.
--- js/scripts.js
+++ js/scripts.js
@@ -287,9 +287,9 @@ $("#reinhard").mouseleave(function(){
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.0001}% ${50 - (_mouseY - _h) * 0.0001}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.0002}% ${50 - (_mouseY - _h) * 0.0002}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.0006}% ${50 - (_mouseY - _h) * 0.0006}%`;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.001}% ${50 - (_mouseY - _h) * 0.001}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.002}% ${50 - (_mouseY - _h) * 0.002}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.006}% ${50 - (_mouseY - _h) * 0.0006}%`;
         let x = `${_depth3}, ${_depth2}, ${_depth1}`;
         //console.log(x);
         elem.style.backgroundPosition = x;
